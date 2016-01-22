7d7d5f869bf7a9c2a8894e681792ea9efacfc89e
Ackara <ackara.dev@outlook.com>
2016-01-21 21:23:33 -0400

    add nuget build scripts

---
+++ build/nuget-publish.ps1
@@ -0,0 +1,47 @@
+﻿# ----------------------------------------------------------------------
+# This script generates .nuspec for each .csproj in the project
+# in the directory excluding the test projects.
+# ----------------------------------------------------------------------
+Clear-Host;
+$publishPackages = $true;
+$projectRootDir = [System.IO.Path]::GetDirectoryName($PSScriptRoot);
+$nuget = [System.IO.Path]::Combine([System.IO.Path]::GetTempPath(), "nuget.exe");
+
+# Download nuget.exe
+& $([System.IO.Path]::Combine($PSScriptRoot, "get-nuget.ps1"));
+
+# Generate and publish .nupkg files
+foreach($path in [System.IO.Directory]::GetFiles($projectRootDir, "*.csproj", [System.IO.SearchOption]::AllDirectories))
+{
+    $fileName = [System.IO.Path]::GetFileName($path);
+    if(($fileName.StartsWith("Test") -eq $false) -and ($fileName.Contains(".UWP.") -eq $false))
+    {
+        Set-Location $([System.IO.Path]::GetDirectoryName($path));
+        & $nuget pack $($fileName) -Prop Configuration=Release -IncludeReferencedProjects;
+        
+        # Publish .nupkg
+        if($publishPackages -eq $true)
+        {
+            $nupkg = Get-Nupkg([System.IO.Path]::GetDirectoryName($path));
+            & $nuget push $($nupkg);
+            [System.IO.File]::Delete($nupkg);
+        }
+    }
+}
+
+echo "DONE!";
+
+# ----------------------
+# Functions
+# ----------------------
+function Get-Nupkg($dir)
+{
+    [string] $nupkg = $null;
+    foreach($path in [System.IO.Directory]::GetFiles($dir, "*.nupkg", [System.IO.SearchOption]::TopDirectoryOnly))
+    {
+        $nupkg = $path;
+        break;
+    }
+
+    return $nupkg;
+}
---
+++ build/nuget-spec.ps1
@@ -0,0 +1,25 @@
+﻿# ----------------------------------------------------------------------
+# This script generates .nuspec for each .csproj in the project
+# in the directory excluding the test projects.
+# ----------------------------------------------------------------------
+Clear-Host;
+$projectRootDir = [System.IO.Path]::GetDirectoryName($PSScriptRoot);
+$nuget = [System.IO.Path]::Combine([System.IO.Path]::GetTempPath(), "nuget.exe");
+
+# Download nuget.exe
+& $([System.IO.Path]::Combine($PSScriptRoot, "get-nuget.ps1"));
+
+# Generate .nuspec files
+foreach($path in [System.IO.Directory]::GetFiles($projectRootDir, "*.csproj", [System.IO.SearchOption]::AllDirectories))
+{
+    $fileName = [System.IO.Path]::GetFileName($path);
+    if($fileName.StartsWith("Test") -eq $false)
+    {
+        $specFile = [System.IO.Path]::ChangeExtension($path, ".nuspec");
+        if([System.IO.File]::Exists($specFile) -eq $false)
+        {
+            Set-Location $([System.IO.Path]::GetDirectoryName($path));
+            & $nuget spec;
+        }
+    }
+}
