23df903b09e6523f6347f41deaa38c5cdb5e5140
Ackara <ackara.dev@outlook.com>
2016-06-11 13:58:51 -0400

    Refactor SqlCommandTests.cs
    * Rename test names.
    * Dynamically fail test if it was unable to created the required sample database.
       In 'debug' mode the test will be ignored in 'release mode it will fail.

--- src/Tests/Tests.Daterpillar/IntegrationTest/SqlCommandTests.cs
+++ src/Tests/Tests.Daterpillar/IntegrationTest/SqlCommandTests.cs
@@ -3,48 +3,31 @@ using Gigobyte.Daterpillar.Data.Linq;
 using Gigobyte.Daterpillar.Transformation;
 using Gigobyte.Daterpillar.Transformation.Template;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System.Collections.Generic;
 using System.Configuration;
-using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using Tests.Daterpillar.Sample;
 
 namespace Tests.Daterpillar.IntegrationTest
 {
     [TestClass]
-    [DeploymentItem(Artifact.SampleSchema)]
-    [DeploymentItem(Artifact.TSqlSampleSchema)]
     public class SqlCommandTests
     {
         [ClassInitialize]
         public static void Setup(TestContext context)
         {
-            try
+            var schema = Schema.Load(SampleData.GetFile(SampleData.MusicxddlXML).OpenRead());
+            _connectionString = ConfigurationManager.ConnectionStrings["mssql"].ConnectionString.Trim();
+            _unableToRunTests = !SampleData.TryCreateSampleDatabase(new SqlConnection(_connectionString), schema, new SqlTemplate(new SqlTemplateSettings()
             {
-                var settings = new SqlTemplateSettings()
-                {
-                    AddScript = true,
-                    UseDatabase = true,
-                    CreateSchema = true,
-                    CommentsEnabled = true,
-                    DropDatabaseIfExist = false,
-                };
+                AddScript = true,
+                CreateSchema = true,
+                UseDatabase = true,
+                CommentsEnabled = false,
+                DropDatabaseIfExist = true,
+            }));
 
-                var schema = Schema.Load(SampleData.GetFile(Artifact.SampleSchema).OpenRead());
-                var script = new SqlTemplate(settings).Transform(schema);
-
-                using (var connection = new System.Data.SqlClient.SqlConnection(ConfigurationManager.ConnectionStrings["mssql"].ConnectionString))
-                {
-                    connection.Open();
-
-                    using (var command = connection.CreateCommand())
-                    {
-                        command.CommandText = script;
-                        command.ExecuteNonQuery();
-                    }
-                }
-            }
-            catch (System.Data.Common.DbException) { }
+            _connectionString += (_connectionString.Last() == ';' ? string.Empty : ";") + $"database={schema.Name}";
         }
 
         [TestMethod]
@@ -52,104 +35,93 @@ namespace Tests.Daterpillar.IntegrationTest
         [TestCategory(Category.Integration)]
         public void Execute_should_retrieve_query_results_from_sql_server_database()
         {
+            IgnoreTestIfDbConnectionIsUnavailable();
+
             // Arrange
-            int limit = 100;
-            var query = new Query(QueryStyle.TSQL).SelectAll().Top(limit).From(Song.Table);
-
-            IList<Song> album;
-            Song track1, single;
-
-            var sut = new AdoNetConnectionWrapper(CreateConnection(true));
-
-            // Act
-            try
+            using (var sut = new AdoNetConnectionWrapper(new SqlConnection(_connectionString), QueryStyle.TSQL))
             {
-                album = new List<Song>(sut.Execute<Song>(query));
-                track1 = album.First();
+                var limit = 100;
 
-                single = sut.Execute<Song>(track1.ToSelectCommand())
+                var query = new Query()
+                    .SelectAll()
+                    .From(Song.Table)
+                    .Where($"{Song.IdColumn}<='{limit}'");
+
+                // Act
+                var album = sut.Execute<Song>(query);
+                var track1 = album.First();
+
+                var single = sut.Execute<Song>(track1.ToSelectCommand())
                     .First();
-            }
-            finally
-            {
-                sut.Dispose();
-            }
 
-            // Assert
-            Assert.AreEqual(limit, album.Count);
-            Assert.AreEqual(track1.Id, single.Id);
+                // Assert
+                Assert.AreEqual(limit, album.Count());
+                Assert.AreEqual(track1.Id, single.Id);
+            }
         }
 
         [TestMethod]
         [Owner(Dev.Ackara)]
         [TestCategory(Category.Integration)]
-        public void Insert_should_commit_add_a_new_record_to_a_sql_server_database()
+        public void Commit_should_execute_a_insert_command_against_sql_server_database()
         {
+            IgnoreTestIfDbConnectionIsUnavailable();
+
             // Arrange
-            Song song;
-            var nameOfTrack = nameof(Insert_should_commit_add_a_new_record_to_a_sql_server_database);
-            var track1 = SampleData.CreateSong(nameOfTrack);
-            var query = new Query().SelectAll().From($"[zune].[dbo].[{Song.Table}]")
-                .Where($"{Song.NameColumn}='{track1.Name}'");
-
-            var sut = new AdoNetConnectionWrapper(CreateConnection(true), QueryStyle.TSQL);
-            System.Diagnostics.Debug.WriteLine(query.GetValue());
-            // Act
-            try
-            {
-                sut.Insert(track1);
-                sut.Commit();
-
-                song = sut.Execute<Song>(query)
-                    .First();
-            }
-            finally
-            {
-                sut.Dispose();
-            }
-
-            // Assert
-            Assert.AreEqual(nameOfTrack, song.Name);
-            Assert.AreNotEqual(track1.Id, song.Id);
-        }
-        
-        [TestMethod]
-        [Owner(Dev.Ackara)]
-        [TestCategory(Category.Integration)]
-        public void Delete_should_remove_a_old_record_from_a_sql_server_database()
-        {
-            // Arrange
-            Song insertedSong;
-            bool trackDeleted;
-            var track1 = SampleData.CreateSong();
-
-            var query = new Query().SelectAll().From($"[zune].[dbo].[{Song.Table}]")
+            var trackName = nameof(Commit_should_execute_a_insert_command_against_sql_server_database);
+            var track1 = SampleData.CreateSong(trackName);
+            var query = new Query()
+                .SelectAll()
+                .From(Song.Table)
                 .Where($"{Song.NameColumn}={track1.Name.ToSQL()}");
 
-            var sut = new AdoNetConnectionWrapper(CreateConnection(true), QueryStyle.TSQL);
-            System.Diagnostics.Debug.WriteLine(query.GetValue());
-            // Act
-            try
+            using (var connection = new AdoNetConnectionWrapper(new SqlConnection(_connectionString), QueryStyle.TSQL))
             {
-                sut.Insert(track1);
-                sut.Commit();
+                // Act
+                connection.Insert(track1);
+                connection.Commit();
 
-                insertedSong = sut.Execute<Song>(query)
+                var song = connection.Execute<Song>(query)
                     .First();
 
-                sut.Delete(insertedSong);
-                sut.Commit();
-
-                trackDeleted = sut.Execute<Song>(query).FirstOrDefault() == null;
+                // Assert
+                Assert.AreEqual(trackName, song.Name);
+                Assert.AreNotEqual(track1.Id, song.Id);
             }
-            finally
+        }
+
+        [TestMethod]
+        [Owner(Dev.Ackara)]
+        [TestCategory(Category.Integration)]
+        public void Commit_should_execute_a_delete_command_against_sql_server_database()
+        {
+            IgnoreTestIfDbConnectionIsUnavailable();
+
+            // Arrange
+            var track1 = SampleData.CreateSong();
+            var query = new Query()
+                .SelectAll()
+                .From(Song.Table)
+                .Where($"{Song.NameColumn}={track1.Name.ToSQL()}");
+
+            using (var connection = new AdoNetConnectionWrapper(new SqlConnection(_connectionString), QueryStyle.TSQL))
             {
-                sut.Dispose();
-            }
+                // Act
+                connection.Insert(track1);
+                connection.Commit();
 
-            // Assert
-            Assert.IsNotNull(insertedSong);
-            Assert.IsTrue(trackDeleted);
+                var insertedTrack = connection.Execute<Song>(query)
+                    .First();
+
+                connection.Delete(insertedTrack);
+                connection.Commit();
+
+                bool trackDeleted = connection.Execute<Song>(query).FirstOrDefault() == null;
+
+                // Assert
+                Assert.IsNotNull(insertedTrack);
+                Assert.IsTrue(trackDeleted);
+            }
         }
 
         #region Private Members
@@ -161,26 +133,13 @@ namespace Tests.Daterpillar.IntegrationTest
         {
             if (_unableToRunTests)
             {
-                string failureMessage = $"The {nameof(SampleData.TryCreateSampleDatabase)}() method was unable to create a sample database.";
+                string failureMessage = $"The {nameof(SampleData)}.{nameof(SampleData.TryCreateSampleDatabase)}() method was unable to create a sample database.";
 #if DEBUG
                 Assert.Inconclusive(failureMessage);
 #else
                 Assert.Fail(failureMessage);
 #endif
             }
-        }
-
-        private static IDbConnection CreateConnection(bool selectDatabase = false)
-        {
-            string connStr = ConfigurationManager.ConnectionStrings["mssql"].ConnectionString;
-            var conn = new System.Data.SqlClient.SqlConnection(connStr);
-            if (selectDatabase)
-            {
-                conn.Open();
-                conn.ChangeDatabase("zune");
-            }
-
-            return conn;
         }
 
         #endregion Private Members
