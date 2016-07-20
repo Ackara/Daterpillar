7c4b75dc7cd6faad8adedfad5ff4669de5c48243
Ackara <ackara.dev@outlook.com>
2016-07-19 21:11:05 -0400

    Add test methods for schema comparer

---
+++ src/Tests.Daterpillar/IntegrationTest/SchemaComparerTests.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Gigobyte.Daterpillar.Automation;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests.Daterpillar.IntegrationTest
+{
+    [TestClass]
+    public class SchemaComparerTests
+    {
+        
+    }
+}
---
+++ src/Tests.Daterpillar/UnitTest/SchemaComparerBaseTests.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Telerik.JustMock;
+using Telerik.JustMock.Helpers;
+using Gigobyte.Daterpillar.Automation;
+using System.Linq;
+using Gigobyte.Daterpillar.Transformation;
+using System.Runtime.Serialization;
+
+namespace Tests.Daterpillar.UnitTest
+{
+    [TestClass]
+    public class SchemaComparerBaseTests
+    {
+        [TestMethod]
+        [Owner(Dev.Ackara)]
+        public void TestMethod1()
+        {
+            // Arrange
+            var serializer = new DataContractSerializer(typeof(Schema));
+            var source = serializer.ReadObject(SampleData.GetFile("").OpenRead());
+            var target = serializer.ReadObject(SampleData.GetFile("").OpenRead());
+            
+            var sourceMockAggregator = Mock.Create<ISchemaAggregator>();
+            sourceMockAggregator.Arrange(x => x.FetchSchema())
+                .Returns(source)
+                .OccursOnce();
+
+            var targetMockAggregator = Mock.Create<ISchemaAggregator>();
+            targetMockAggregator.Arrange(x => x.FetchSchema())
+                .Returns(target)
+                .OccursOnce();
+
+            var sut = Mock.Create<SchemaComparerBase>();
+
+            // Act
+            var report = sut.Compare(sourceMockAggregator, targetMockAggregator);
+
+            // Assert
+            Assert.AreEqual(0, report.Discrepancies.Count);
+            Assert.AreEqual(Outcome.Equal, report.Summary);
+        }
+
+        [TestMethod]
+        [Owner(Dev.Ackara)]
+        public void MyTestMethod()
+        {
+            // Arrange
+            var sut = Mock.Create<SchemaComparerBase>();
+
+            // Act
+            var report = sut.Compare(null, null);
+
+            // Assert
+            Assert.IsTrue(report.Discrepancies.Count > 0);
+            Assert.AreEqual(Outcome.NotEqual, report.Summary);
+            Assert.AreEqual(Outcome.SourceEmpty, report.Summary);
+        }
+
+        [TestMethod]
+        [Owner(Dev.Ackara)]
+        public void MyTestMethod2()
+        {
+            // Arrange
+            var sut = Mock.Create<SchemaComparerBase>();
+
+            // Act
+            var report = sut.Compare(null, null);
+
+            // Assert
+            Assert.AreEqual(Outcome.NotEqual, report.Summary);
+            Assert.AreEqual(Outcome.TargetEmpty, report.Summary);
+            CollectionAssert.AllItemsAreNotNull(report.Discrepancies.ToArray());
+        }
+
+        
+    }
+}
