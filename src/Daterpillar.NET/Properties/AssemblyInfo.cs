69a1ba9c9d4da6bc1b3ff22e25085fe9b0d56e01
Ackara <ackara.dev@outlook.com>
2016-07-19 21:19:17 -0400

    Rename Daterpillar.DotNet project to Daterpillar.NET

--- src/Daterpillar.NET/Properties/AssemblyInfo.cs
+++ src/Daterpillar.NET/Properties/AssemblyInfo.cs
@@ -1,37 +1,36 @@
-﻿using System;
-using System.Reflection;
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following
+// General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Daterpillar.DotNet")]
+[assembly: AssemblyTitle("Daterpillar.NET")]
 [assembly: AssemblyDescription("Daterpillar is a data access and transformation technology for new applications.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Gigobyte")]
-[assembly: AssemblyProduct("Daterpillar.DotNet")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyProduct("Daterpillar.NET")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: CLSCompliantAttribute(true)]
 
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("7178415e-3fbc-4e66-a897-1bd5b92a5a9a")]
+[assembly: Guid("785e04bf-f296-481b-983a-e3efba3e8f06")]
 
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
-//      Minor Version
+//      Minor Version 
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers
+// You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.2.16199.107")]
-[assembly: AssemblyFileVersion("1.2.16199.107")]
+[assembly: AssemblyVersion("1.2.16202.109")]
+[assembly: AssemblyFileVersion("1.2.16202.109")]
