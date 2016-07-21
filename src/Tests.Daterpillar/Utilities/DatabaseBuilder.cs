3fac25db1c85bc3112b43332af3af10e411ad63d
Ackara <ackara.dev@outlook.com>
2016-07-21 07:01:11 -0400

    Add DatabaseBuilder.cs

---
+++ src/Tests.Daterpillar/Utilities/DatabaseBuilder.cs
@@ -0,0 +1,13 @@
+﻿using Gigobyte.Daterpillar.Transformation;
+using System.Data;
+
+namespace Tests.Daterpillar.Utilities
+{
+    public static class DatabaseBuilder
+    {
+        public static bool TryCreateDatabase(IDbConnection connection, Schema schema)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
