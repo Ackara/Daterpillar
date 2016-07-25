c16b95fe9f1fd5a847e3919426982c053c3f48e4
Ackara <ackara.dev@outlook.com>
2016-07-25 07:33:52 -0400

    Add new overload to DatabaseBuilder TryCreateDatabase method

--- src/Tests.Daterpillar/Utilities/DatabaseBuilder.cs
+++ src/Tests.Daterpillar/Utilities/DatabaseBuilder.cs
@@ -1,11 +1,21 @@
 ﻿using Gigobyte.Daterpillar.Transformation;
 using Gigobyte.Daterpillar.Transformation.Template;
+using System;
 using System.Data;
 
 namespace Tests.Daterpillar.Utilities
 {
     public static class DatabaseBuilder
     {
+        public static bool TryResetDatabase(string connectionName, ITemplate template)
+        {
+            var settings = System.Configuration.ConfigurationManager.ConnectionStrings[connectionName];
+            var connection = (IDbConnection)Activator.CreateInstance(Type.GetType(settings.ProviderName));
+            var schema = Schema.Load(Test.Data.GetFile(Test.File.DataSoftXDDL).OpenRead());
+
+            return TryResetDatabase(connection, schema, template);
+        }
+
         public static bool TryResetDatabase(IDbConnection connection, ITemplate template)
         {
             var schema = Schema.Load(Test.Data.GetFile(Test.File.DataSoftXDDL).OpenRead());
