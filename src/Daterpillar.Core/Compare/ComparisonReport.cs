4fb93463f5d493c8c5939006ff4763026972c960
Ackara <ackara.dev@outlook.com>
2016-07-26 07:37:15 -0400

    Add data contract attributes to ComparisionReport.cs

--- src/Daterpillar.Core/Compare/ComparisonReport.cs
+++ src/Daterpillar.Core/Compare/ComparisonReport.cs
@@ -1,13 +1,29 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
 
 namespace Gigobyte.Daterpillar.Compare
 {
-    public class ComparisonReport
+    [DataContract]
+    public class ComparisonReport : IEnumerable<Discrepancy>
     {
         public Counter Counters;
 
+        [DataMember]
         public Outcome Summary { get; set; }
+
+        [DataMember]
         public IList<Discrepancy> Discrepancies { get; set; }
+
+        public IEnumerator<Discrepancy> GetEnumerator()
+        {
+            foreach (var item in Discrepancies) { yield return item; }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
 
         public struct Counter
         {
