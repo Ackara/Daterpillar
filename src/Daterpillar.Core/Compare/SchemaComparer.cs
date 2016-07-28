4512dbe01031cba35234c5a2477427ae88a6ab4f
Ackara <ackara.dev@outlook.com>
2016-07-28 07:35:34 -0400

    Add implementation detail to SchemaComparers

--- src/Daterpillar.Core/Compare/SchemaComparer.cs
+++ src/Daterpillar.Core/Compare/SchemaComparer.cs
@@ -1,5 +1,4 @@
 ﻿using Gigobyte.Daterpillar.Transformation;
-using System;
 
 namespace Gigobyte.Daterpillar.Compare
 {
@@ -19,7 +18,11 @@ namespace Gigobyte.Daterpillar.Compare
         public ComparisonReport GenerateReport(Schema source, Schema target)
         {
             _report = new ComparisonReport();
+            _report.TotalSourceTables = source.Tables.Count;
+            _report.TotalTargetTables = target.Tables.Count;
+
             FindDiscrepanciesBetween(source.Tables.ToArray(), target.Tables.ToArray());
+            _report.Summarize();
             return _report;
         }
 
@@ -37,29 +40,52 @@ namespace Gigobyte.Daterpillar.Compare
         private static void EnsureBothArraysAreOfTheSameSize<T>(ref T[] left, ref T[] right)
         {
             if (left.Length > right.Length)
-                IncreaseLengthOfArray(right, left.Length);
+                IncreaseLengthOfArray(ref right, left.Length);
             else if (left.Length < right.Length)
-                IncreaseLengthOfArray(left, right.Length);
+                IncreaseLengthOfArray(ref left, right.Length);
         }
 
-        private static void IncreaseLengthOfArray<T>(T[] array, int capacity)
+        private static void IncreaseLengthOfArray<T>(ref T[] array, int capacity)
         {
-            throw new NotImplementedException();
+            var newArray = new T[capacity];
+            for (int i = 0; i < array.Length; i++)
+            {
+                newArray[i] = array[i];
+            }
+
+            array = newArray;
         }
 
-        private static void AlignTheElementsOfBothArraysByName<T>(ref T[] left, ref T[] right)
+        private static void AlignTheItemsOfBothArraysByName<T>(ref T[] left, ref T[] right)
         {
-            dynamic l, r;
+            dynamic l;
             for (int i = 0; i < left.Length; i++)
             {
-                l = left[i]; r = right[i];
+                l = left[i];
+                SwapMactchingItems(ref right, (l?.Name), i);
+            }
+        }
+
+        private static void SwapMactchingItems<T>(ref T[] right, string name, int targetIdx)
+        {
+            dynamic r; T temp;
+            for (int i = targetIdx; i < right.Length; i++)
+            {
+                r = right[i];
+                if (name == r?.Name)
+                {
+                    temp = right[targetIdx];
+                    right[targetIdx] = right[i];
+                    right[i] = temp;
+                    break;
+                }
             }
         }
 
         private void FindDiscrepanciesBetween(Table[] left, Table[] right)
         {
             EnsureBothArraysAreOfTheSameSize(ref left, ref right);
-            AlignTheElementsOfBothArraysByName(ref left, ref right);
+            AlignTheItemsOfBothArraysByName(ref left, ref right);
 
             for (int i = 0; i < left.Length; i++)
             {
@@ -70,11 +96,33 @@ namespace Gigobyte.Daterpillar.Compare
         private void FindDiscrepanciesBetween(Column[] left, Column[] right)
         {
             EnsureBothArraysAreOfTheSameSize(ref left, ref right);
-            AlignTheElementsOfBothArraysByName(ref left, ref right);
+            AlignTheItemsOfBothArraysByName(ref left, ref right);
 
             // TODO: compare each column.
             for (int i = 0; i < left.Length; i++)
             {
+                if (left[i].Name != right[i].Name)
+                {
+                    _report.Discrepancies.Add(new Discrepancy()
+                    {
+                    });
+                }
+
+                if (left[i].AutoIncrement != right[i].AutoIncrement)
+                {
+                    _report.Discrepancies.Add(new Discrepancy()
+                    {
+                    });
+                }
+
+                if (left[i].DataType == right[i].DataType)
+                {
+                    _report.Discrepancies.Add(new Discrepancy()
+                    {
+                    });
+                }
+
+
             }
         }
 
