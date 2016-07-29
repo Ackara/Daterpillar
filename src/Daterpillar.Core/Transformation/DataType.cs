d4097c07f9f21f5936b34a7808f0457ee0a14793
Ackara <ackara.dev@outlook.com>
2016-07-29 06:26:39 -0400

    Cleanup code

--- src/Daterpillar.Core/Transformation/DataType.cs
+++ src/Daterpillar.Core/Transformation/DataType.cs
@@ -3,7 +3,9 @@ using System.Xml.Serialization;
 
 namespace Gigobyte.Daterpillar.Transformation
 {
-    /// <summary>Represents a SQL type.</summary>
+    /// <summary>
+    /// Represents a SQL type.
+    /// </summary>
     public struct DataType : IEquatable<DataType>
     {
         #region Operators
@@ -20,24 +22,32 @@ namespace Gigobyte.Daterpillar.Transformation
 
         #endregion Operators
 
-        /// <summary>Initializes a new instance of the <see cref="DataType"/> struct.</summary>
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DataType"/> struct.
+        /// </summary>
         /// <param name="typeName">Name of the type.</param>
         public DataType(string typeName) : this()
         {
             Name = typeName;
         }
 
-        /// <summary>Gets or sets the scale.</summary>
+        /// <summary>
+        /// Gets or sets the scale.
+        /// </summary>
         /// <value>The scale.</value>
         [XmlAttribute("scale")]
         public int Scale { get; set; }
 
-        /// <summary>Gets or sets the precision.</summary>
+        /// <summary>
+        /// Gets or sets the precision.
+        /// </summary>
         /// <value>The precision.</value>
         [XmlAttribute("precision")]
         public int Precision { get; set; }
 
-        /// <summary>Gets or sets the name.</summary>
+        /// <summary>
+        /// Gets or sets the name.
+        /// </summary>
         /// <value>The name.</value>
         [XmlText]
         public string Name { get; set; }
@@ -55,7 +65,7 @@ namespace Gigobyte.Daterpillar.Transformation
 
         public override int GetHashCode()
         {
-            return Name.GetHashCode() ^ Precision ^ Scale;
+            return Name.GetHashCode() ^ Precision.GetHashCode() ^ Scale.GetHashCode();
         }
     }
 }
