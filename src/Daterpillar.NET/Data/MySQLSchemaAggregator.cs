3c36db13361ebb8037e0d99f577df4f55ebd767b
Ackara <ackara.dev@outlook.com>
2016-08-07 17:25:50 -0400

    Add mysql and sqlite schema aggregators

---
+++ src/Daterpillar.NET/Data/MySQLSchemaAggregator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Data;
+
+namespace Gigobyte.Daterpillar.Data
+{
+    public class MySQLSchemaAggregator : SchemaAggregatorBase
+    {
+        public MySQLSchemaAggregator(IDbConnection connection) : base(connection)
+        {
+        }
+
+        protected override string GetColumnInfoQuery(string tableName)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override string GetForeignKeyInfoQuery(string tableName)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override string GetIndexColumnsQuery(string indexIdentifier)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override string GetIndexInfoQuery(string tableName)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override string GetTableInfoQuery()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/Daterpillar.NET/Data/SQLiteSchemaAggregator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Data;
+
+namespace Gigobyte.Daterpillar.Data
+{
+    public class SQLiteSchemaAggregator : SchemaAggregatorBase
+    {
+        public SQLiteSchemaAggregator(IDbConnection connection) : base(connection)
+        {
+        }
+
+        protected override string GetColumnInfoQuery(string tableName)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override string GetForeignKeyInfoQuery(string tableName)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override string GetIndexColumnsQuery(string indexIdentifier)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override string GetIndexInfoQuery(string tableName)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override string GetTableInfoQuery()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
