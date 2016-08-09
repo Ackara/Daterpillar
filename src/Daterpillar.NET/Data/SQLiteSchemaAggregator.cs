285f8b1bc4596052e0ed37ac867b9859ce731249
Ackara <ackara.dev@outlook.com>
2016-08-09 07:38:18 -0400

    Add implementation details to SQLiteSchema Aggregator

--- src/Daterpillar.NET/Data/SQLiteSchemaAggregator.cs
+++ src/Daterpillar.NET/Data/SQLiteSchemaAggregator.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Data;
+using Gigobyte.Daterpillar.Transformation;
+using System.Text.RegularExpressions;
 
 namespace Gigobyte.Daterpillar.Data
 {
@@ -9,14 +11,47 @@ namespace Gigobyte.Daterpillar.Data
         {
         }
 
+        protected override void LoadColumnInformationIntoSchema(Table table, DataTable columnInfo)
+        {
+            foreach (DataRow row in columnInfo.Rows)
+            {
+                string temp;
+
+                string typeName = Convert.ToString(row[ColumnName.Type]);
+                typeName = (typeName == "INTEGER" ? "int" : typeName.ToLower());
+
+                temp = _dataTypeRegex.Match(typeName)?.Groups["scale"]?.Value;
+                int scale = Convert.ToInt32((string.IsNullOrEmpty(temp) ? "0" : temp));
+
+                temp = _dataTypeRegex.Match(typeName)?.Groups["precision"]?.Value;
+                int precision = Convert.ToInt32((string.IsNullOrEmpty(temp) ? "0" : temp));
+
+                string defaultValue = Convert.ToString(row["dflt_value"]);
+
+                var newColumn = new Column();
+                newColumn.Name = Convert.ToString(row[ColumnName.Name]);
+                newColumn.DataType = new DataType(typeName, scale, precision);
+                //newColumn.AutoIncrement = Convert.ToBoolean(row[ColumnName.Auto]);
+                newColumn.IsNullable = !Convert.ToBoolean(row["notnull"]);
+                if (!string.IsNullOrEmpty(defaultValue)) newColumn.Modifiers.Add(defaultValue);
+
+                table.Columns.Add(newColumn);
+            }
+        }
+
+        protected override void LoadForeignKeyInformationIntoSchema(Table table, DataTable foreignKeyInfo)
+        {
+            base.LoadForeignKeyInformationIntoSchema(table, foreignKeyInfo);
+        }
+
         protected override string GetColumnInfoQuery(string tableName)
         {
-            throw new NotImplementedException();
+            return $"PRAGMA table_info('{tableName}');";
         }
 
         protected override string GetForeignKeyInfoQuery(string tableName)
         {
-            throw new NotImplementedException();
+            return $"PRAGMA foreign_key_list('{tableName}');";
         }
 
         protected override string GetIndexColumnsQuery(string indexIdentifier)
@@ -31,7 +66,11 @@ namespace Gigobyte.Daterpillar.Data
 
         protected override string GetTableInfoQuery()
         {
-            throw new NotImplementedException();
+            return $"select sm.tbl_name AS [Name], '' AS [Comment] from sqlite_master sm WHERE sm.sql IS NOT NULL AND sm.name <> 'sqlite_sequence' AND sm.type = 'table';";
         }
+
+        #region Private Members
+        private Regex _dataTypeRegex = new Regex(@"\((?<scale>\d+),? ?(?<precision>\d+)\)", RegexOptions.Compiled);
+        #endregion
     }
 }
