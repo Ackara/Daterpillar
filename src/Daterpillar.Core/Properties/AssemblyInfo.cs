ca05436968374d3d4ee4d91bea4b3fd0defb412a
Ackara <ackara.dev@outlook.com>
2016-08-22 07:13:28 -0400

    Update assembly version

--- src/Daterpillar.Core/Properties/AssemblyInfo.cs
+++ src/Daterpillar.Core/Properties/AssemblyInfo.cs
@@ -20,5 +20,5 @@ using System.Resources;
 //
 // You can specify all the values or you can default the Build and Revision Numbers by using the '*'
 // as shown below: [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.1.16230.220")]
-[assembly: AssemblyFileVersion("2.1.16230.220")]
+[assembly: AssemblyVersion("2.1.16233.221")]
+[assembly: AssemblyFileVersion("2.1.16233.221")]
