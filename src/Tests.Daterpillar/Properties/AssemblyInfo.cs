84ba0e4758b4f856bdd8fd87ffcb74c914378024
Ackara <ackara.dev@outlook.com>
2016-08-30 06:44:20 -0400

    Update assembly version

--- src/Tests.Daterpillar/Properties/AssemblyInfo.cs
+++ src/Tests.Daterpillar/Properties/AssemblyInfo.cs
@@ -25,5 +25,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers by using the '*'
 // as shown below: [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.16241.337")]
-[assembly: AssemblyFileVersion("1.1.16241.337")]
+[assembly: AssemblyVersion("1.1.16243.338")]
+[assembly: AssemblyFileVersion("1.1.16243.338")]
