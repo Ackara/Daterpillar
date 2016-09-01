70332b0e4ac0e24ecb5ef9cea110d0ae27fd7af6
Ackara <ackara.dev@outlook.com>
2016-09-01 07:02:48 -0400

    Add Files
    * ISynchronizer
    * SynchronizerBase
    * IScriptBuilder

---
+++ src/Daterpillar.Core/Migration/ISynchronizer.cs
@@ -0,0 +1,14 @@
+﻿using Gigobyte.Daterpillar.TextTransformation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gigobyte.Daterpillar.Migration
+{
+    public interface ISynchronizer
+    {
+        byte[] GenerateScript(Schema schema);
+    }
+}
---
+++ src/Daterpillar.Core/TextTransformation/IScriptBuilder.cs
@@ -0,0 +1,15 @@
+﻿namespace Gigobyte.Daterpillar.TextTransformation
+{
+    public interface IScriptBuilder
+    {
+        void Create(Table table);
+
+        void Create(Index index);
+
+        void Drop(Table table);
+
+        void Drop(Index index);
+
+        void AlterTable(Table tableA, Table tableB);
+    }
+}
