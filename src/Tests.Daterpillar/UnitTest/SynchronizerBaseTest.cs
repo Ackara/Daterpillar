9709c7d5ae9b04e456ffcceb8dee89d0b32cf692
Ackara <ackara.dev@outlook.com>
2016-09-02 07:32:19 -0400

    Add synchronization test

---
+++ src/Tests.Daterpillar/UnitTest/SynchronizerBaseTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests.Daterpillar.UnitTest
+{
+    [TestClass]
+    public class SynchronizerBaseTest
+    {
+        [TestMethod]
+        [Owner(Test.Dev.Ackara)]
+        public void TestMethod1()
+        {
+        }
+    }
+}
