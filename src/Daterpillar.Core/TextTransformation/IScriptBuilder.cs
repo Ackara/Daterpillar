a20cdbb0dc44d651c0e216de73cdd7929a1bdc72
Ackara <ackara.dev@outlook.com>
2016-09-02 16:11:31 -0400

    Add methods to IScriptBuilder

--- src/Daterpillar.Core/TextTransformation/IScriptBuilder.cs
+++ src/Daterpillar.Core/TextTransformation/IScriptBuilder.cs
@@ -2,14 +2,26 @@
 {
     public interface IScriptBuilder
     {
+        void Append(string text);
+        
+        void AppendLine(string text);
+
         void Create(Table table);
 
         void Create(Index index);
+
+        void Create(ForeignKey foreignKey);
 
         void Drop(Table table);
 
         void Drop(Index index);
 
+        void Drop(ForeignKey foreignKey);
+
         void AlterTable(Table tableA, Table tableB);
+                
+        byte[] GetContentAsBytes();
+
+        string GetContentAsString();
     }
 }
