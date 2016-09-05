8fea9f79463190ef7a67b2f722f17f65e0994266
Ackara <ackara.dev@outlook.com>
2016-09-05 10:10:39 -0400

    Override datatype.cs ToString method

--- src/Daterpillar.Core/DataType.cs
+++ src/Daterpillar.Core/DataType.cs
@@ -103,5 +103,14 @@ namespace Gigobyte.Daterpillar
         {
             return Name.GetHashCode() ^ Precision.GetHashCode() ^ Scale.GetHashCode();
         }
+
+        /// <summary>
+        /// Returns a <see cref="System.String"/> that represents this instance.
+        /// </summary>
+        /// <returns>A <see cref="System.String"/> that represents this instance.</returns>
+        public override string ToString()
+        {
+            return $"{Name}({Scale}, {Precision})";
+        }
     }
 }
