783c449dea0f53637e3c1883afbd764fed5a74d7
Ackara <ackara.dev@outlook.com>
2016-09-07 09:44:59 -0400

    Add ColumnEqualityComparer.cs

---
+++ src/Daterpillar.Core/Equality/ColumnEqualityComparer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+namespace Gigobyte.Daterpillar.Equality
+{
+    public class ColumnEqualityComparer : IEqualityComparer<Column>
+    {
+        public bool Equals(Column x, Column y)
+        {
+            if (x == null && y == null) return true;
+            else if (x == null || y == null) return false;
+            else
+            {
+                return x.Name == y.Name
+                && x.DataType == y.DataType
+                && x.IsNullable == y.IsNullable
+                && x.AutoIncrement == y.AutoIncrement
+                && x.Comment == y.Comment
+                && x.OrdinalPosition == y.OrdinalPosition;
+            }
+        }
+
+        public int GetHashCode(Column obj)
+        {
+            return obj.Name.GetHashCode() ^ obj.DataType.GetHashCode() ^ obj.IsNullable.GetHashCode() ^ obj.AutoIncrement.GetHashCode() ^ obj.OrdinalPosition.GetHashCode() ^ obj.Comment.GetHashCode();
+        }
+    }
+}
