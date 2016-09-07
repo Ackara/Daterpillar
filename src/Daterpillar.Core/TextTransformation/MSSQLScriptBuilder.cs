7c1b29e9965337e35dc384ac9b845eb1c263af7a
Ackara <ackara.dev@outlook.com>
2016-09-07 11:33:34 -0400

    Move MSSQLScriptBuilderTest.cs to unitest namespace.

--- src/Daterpillar.Core/TextTransformation/MSSQLScriptBuilder.cs
+++ src/Daterpillar.Core/TextTransformation/MSSQLScriptBuilder.cs
@@ -2,8 +2,71 @@
 
 namespace Gigobyte.Daterpillar.TextTransformation
 {
-    public class MSSQLScriptBuilder
+    public class MSSQLScriptBuilder : IScriptBuilder
     {
+        public void AlterTable(Column oldColumn, Column newColumn)
+        {
+            throw new NotImplementedException();
+        }
 
+        public void AlterTable(Table oldTable, Table newTable)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Append(string text)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AppendLine(string text)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Create(ForeignKey foreignKey)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Create(Index index)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Create(Column column)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Create(Table table)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Drop(Index index)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Drop(ForeignKey foreignKey)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Drop(Column column)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Drop(Table table)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetContent()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
---
+++ src/Tests.Daterpillar/UnitTest/MSSQLScriptBuilderTest.cs
@@ -0,0 +1,228 @@
+﻿using ApprovalTests;
+using ApprovalTests.Namers;
+using ApprovalTests.Reporters;
+using Gigobyte.Daterpillar;
+using Gigobyte.Daterpillar.TextTransformation;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+
+namespace Tests.Daterpillar.UnitTest
+{
+    [TestClass]
+    [UseApprovalSubdirectory(nameof(ApprovalTests))]
+    [UseReporter(typeof(DiffReporter), typeof(ClipboardReporter))]
+    public class MSSQLScriptBuilderTest
+    {
+        [TestMethod]
+        [Owner(Test.Dev.Ackara)]
+        public void Create_should_return_a_tsql_command_for_adding_a_new_table()
+        {
+            // Arrange
+            var sut = new MSSQLScriptBuilder();
+
+            // Act
+            sut.Create(SampleData.CreateTableSchema());
+
+            // Assert
+            Approvals.Verify(sut.GetContent());
+        }
+
+        [TestMethod]
+        [Owner(Test.Dev.Ackara)]
+        public void Create_should_return_a_tsql_command_for_adding_a_new_index()
+        {
+            // Arrange
+            var index = new Index()
+            {
+                Name = "idx1",
+                IndexType = IndexType.Index,
+                Table = "tblA",
+                Unique = true
+            };
+            index.Columns = new List<IndexColumn>(new IndexColumn[] { new IndexColumn("Name"), new IndexColumn("Age", SortOrder.DESC) });
+
+            var sut = new MSSQLScriptBuilder();
+
+            // Act
+            sut.Create(index);
+
+            // Assert
+            Approvals.Verify(sut.GetContent());
+        }
+
+        [TestMethod]
+        [Owner(Test.Dev.Ackara)]
+        public void Create_should_return_a_tsql_command_for_adding_a_foreignKey()
+        {
+            // Arrange
+            var sut = new MSSQLScriptBuilder();
+
+            // Act
+            sut.Create(new ForeignKey()
+            {
+                Name = "fk1",
+                LocalTable = "dept",
+                LocalColumn = "Name",
+                ForeignTable = "location",
+                ForeignColumn = "street",
+                OnDeleteRule = ForeignKeyRule.RESTRICT,
+                OnUpdateRule = ForeignKeyRule.CASCADE
+            });
+
+            // Assert
+            Approvals.Verify(sut.GetContent());
+        }
+
+        [TestMethod]
+        [Owner(Test.Dev.Ackara)]
+        public void Create_should_return_a_tsql_command_for_adding_a_column_to_a_table()
+        {
+            // Arrange
+            var sut = new MSSQLScriptBuilder();
+
+            // Act
+            sut.Create(new Column()
+            {
+                Name = "Name",
+                DataType = new DataType("varchar", 64, 0),
+                Table = "User",
+                Comment = "This is a comment",
+                AutoIncrement = true
+            });
+
+            // Assert
+            Approvals.Verify(sut.GetContent());
+        }
+
+        [TestMethod]
+        [Owner(Test.Dev.Ackara)]
+        public void Drop_should_return_a_tsql_command_for_dropping_a_table()
+        {
+            // Arrange
+            var sut = new MSSQLScriptBuilder();
+
+            // Act
+            sut.Drop(SampleData.CreateTableSchema());
+
+            // Assert
+            Approvals.Verify(sut.GetContent());
+        }
+
+        [TestMethod]
+        [Owner(Test.Dev.Ackara)]
+        public void Drop_should_return_a_tsql_command_for_dropping_an_index()
+        {
+            // Arrange
+            var index = new Index()
+            {
+                Name = "idx1",
+                IndexType = IndexType.Primary,
+                Table = "tblA"
+            };
+            index.Columns = new List<IndexColumn>(new IndexColumn[] { new IndexColumn("Id") });
+
+            var sut = new MSSQLScriptBuilder();
+
+            // Act
+            sut.Drop(index);
+
+            // Assert
+            Approvals.Verify(sut.GetContent());
+        }
+
+        [TestMethod]
+        [Owner(Test.Dev.Ackara)]
+        public void Drop_should_return_a_tsql_command_for_dropping_a_foreignKey()
+        {
+            // Arrange
+            var sut = new MSSQLScriptBuilder();
+
+            // Act
+            sut.Drop(new ForeignKey()
+            {
+                Name = "fk1",
+                LocalTable = "dept",
+                LocalColumn = "Name",
+                ForeignTable = "location",
+                ForeignColumn = "street",
+                OnDeleteRule = ForeignKeyRule.RESTRICT,
+                OnUpdateRule = ForeignKeyRule.CASCADE
+            });
+
+            // Assert
+            Approvals.Verify(sut.GetContent());
+        }
+
+        [TestMethod]
+        [Owner(Test.Dev.Ackara)]
+        public void Drop_should_return_a_tsql_command_for_dropping_a_column()
+        {
+            // Arrange
+            var sut = new MSSQLScriptBuilder();
+
+            // Act
+            sut.Drop(new Column()
+            {
+                Name = "Name",
+                DataType = new DataType("varchar", 64, 0),
+                Table = "User",
+                Comment = "This is a comment",
+                AutoIncrement = true
+            });
+
+            // Assert
+            Approvals.Verify(sut.GetContent());
+        }
+
+        [TestMethod]
+        [Owner(Test.Dev.Ackara)]
+        public void AlterTable_should_return_a_tsql_command_for_altering_a_table()
+        {
+            // Arrange
+            var oldTable = SampleData.CreateTableSchema();
+            var newTable = SampleData.CreateTableSchema("Supervisor");
+            newTable.Columns[1].DataType = new DataType("short", 16, 0);
+
+            var sut = new MSSQLScriptBuilder();
+
+            // Act
+            sut.AlterTable(oldTable, newTable);
+
+            // Assert
+            Approvals.Verify(sut.GetContent());
+        }
+
+        [TestMethod]
+        [Owner(Test.Dev.Ackara)]
+        public void AlterTable_should_return_a_tsql_command_altering_table_column()
+        {
+            // Arrange
+            var oldTable = new Column()
+            {
+                Name = "Name",
+                DataType = new DataType("varchar", 64, 0),
+                Table = "User",
+                Comment = "This is a comment",
+                AutoIncrement = true
+            };
+
+            var newTable = new Column()
+            {
+                Name = "Fullname",
+                DataType = new DataType("varchar", 64, 0),
+                Table = "User",
+                IsNullable = true,
+                Comment = "This is a comment",
+                AutoIncrement = true
+            };
+
+            var sut = new MSSQLScriptBuilder();
+
+            // Act
+            sut.AlterTable(oldTable, newTable);
+
+            // Assert
+            Approvals.Verify(sut.GetContent());
+        }
+    }
+}
