bfebc36b5f8570a8ae78e7c06c7d32f50f30ba12
Ackara <ackara.dev@outlook.com>
2016-09-07 11:32:42 -0400

    Add new contructors to Index.cs

--- src/Daterpillar.Core/IndexColumn.cs
+++ src/Daterpillar.Core/IndexColumn.cs
@@ -8,6 +8,25 @@ namespace Gigobyte.Daterpillar
     public struct IndexColumn
     {
         /// <summary>
+        /// Initializes a new instance of the <see cref="IndexColumn"/> struct.
+        /// </summary>
+        /// <param name="name">The name.</param>
+        public IndexColumn(string name) : this(name, SortOrder.ASC)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="IndexColumn"/> struct.
+        /// </summary>
+        /// <param name="name">The name.</param>
+        /// <param name="order">The order.</param>
+        public IndexColumn(string name, SortOrder order)
+        {
+            Name = name;
+            Order = order;
+        }
+
+        /// <summary>
         /// Gets or sets the column's name.
         /// </summary>
         /// <value>The name.</value>
