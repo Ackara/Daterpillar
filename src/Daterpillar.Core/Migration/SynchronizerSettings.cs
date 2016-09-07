15123cb3bdf3d78b9fa18af371b4832314a79dcc
Ackara <ackara.dev@outlook.com>
2016-09-07 09:42:39 -0400

    Add SynchronizerSetttings.cs

---
+++ src/Daterpillar.Core/Migration/SynchronizerSettings.cs
@@ -0,0 +1,15 @@
+﻿namespace Gigobyte.Daterpillar.Migration
+{
+    public struct SynchronizerSettings
+    {
+        public static SynchronizerSettings Default = new SynchronizerSettings()
+        {
+            IncludeComments = true,
+            IncludeDropCommands = false
+        };
+
+        public bool IncludeComments { get; set; }
+
+        public bool IncludeDropCommands { get; set; }
+    }
+}
