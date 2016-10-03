7e87b075c96b67c2ed074c6593e9d590e86b49d3
Ackara <ackara.dev@outlook.com>
2016-10-03 06:08:12 -0400

    Add implementation details to mssqlscriptbuild.cs

--- src/Daterpillar.Core/TextTransformation/MSSQLScriptBuilder.cs
+++ src/Daterpillar.Core/TextTransformation/MSSQLScriptBuilder.cs
@@ -13,69 +13,67 @@ namespace Gigobyte.Daterpillar.TextTransformation
         {
         }
 
-        public MSSQLScriptBuilder(MSSQLScriptBuilderSettings settings, ITypeNameResolver typeResolver, string schema = null)
+        public MSSQLScriptBuilder(MSSQLScriptBuilderSettings settings, ITypeNameResolver typeResolver)
         {
-            _schemaName = schema;
             _settings = settings;
             _typeResolver = typeResolver;
         }
 
         public void Append(string text)
         {
-            _text.Append(text);
+            _script.Append(text);
         }
 
         public void AppendLine(string text)
         {
-            _text.AppendLine(text);
+            _script.AppendLine(text);
         }
 
         public void Create(Schema schema)
         {
             string lineBreak = "-- ======================================================================";
 
-            _text.AppendLine(lineBreak);
-            _text.AppendLine("-- NAME:");
-            _text.AppendLine($"-- {schema.Name}");
-            _text.AppendLine();
-            _text.AppendLine("-- DESCRIPTION:");
-            _text.AppendLine($"-- {schema.Description}");
-            _text.AppendLine();
-            _text.AppendLine("-- AUTHOR:");
-            _text.AppendLine($"-- {schema.Author}");
-            _text.AppendLine();
-            _text.AppendLine("-- DATE:");
-            _text.AppendLine($"-- {schema.CreatedOn.ToString("ddd dd, yyyy hh:mm tt")}");
-            _text.AppendLine(lineBreak);
-            _text.AppendLine();
+            _script.AppendLine(lineBreak);
+            _script.AppendLine("-- NAME:");
+            _script.AppendLine($"-- {schema.Name}");
+            _script.AppendLine();
+            _script.AppendLine("-- DESCRIPTION:");
+            _script.AppendLine($"-- {schema.Description}");
+            _script.AppendLine();
+            _script.AppendLine("-- AUTHOR:");
+            _script.AppendLine($"-- {schema.Author}");
+            _script.AppendLine();
+            _script.AppendLine("-- DATE:");
+            _script.AppendLine($"-- {schema.CreatedOn.ToString("ddd dd, yyyy hh:mm tt")}");
+            _script.AppendLine(lineBreak);
+            _script.AppendLine();
 
-            _text.AppendLine($"CREATE DATABASE [{schema.Name}];");
-            _text.AppendLine();
+            _script.AppendLine($"CREATE DATABASE [{schema.Name}];");
+            _script.AppendLine();
 
-            _schemaName = (string.IsNullOrEmpty(schema.Name) ? string.Empty : $"[{schema.Name}].");
             foreach (var table in schema.Tables) Create(table);
 
             lineBreak = "-- =================";
-            _text.AppendLine(lineBreak);
-            _text.AppendLine($"-- SCRIPTS (000)");
-            _text.AppendLine(lineBreak);
-            _text.AppendLine();
+            _script.AppendLine(lineBreak);
+            _script.AppendLine($"-- SCRIPTS (000)");
+            _script.AppendLine(lineBreak);
+            _script.AppendLine();
 
-            _text.AppendLine(schema.Script);
+            _script.AppendLine(schema.Script);
         }
 
         public void Create(Table table)
         {
-            _text.AppendLine($"CREATE TABLE {_schemaName}[dbo].[{table.Name}]");
-            _text.AppendLine("(");
+            _script.AppendLine($"CREATE TABLE [{table.SchemaRef.Name}].[dbo].[{table.Name}]");
+            _script.AppendLine("(");
 
             foreach (var column in table.Columns) AppendToTable(column);
             foreach (var constraint in table.ForeignKeys) AppendToTable(constraint);
 
-            _text.Remove((_text.Length - 3), 3);
-            _text.AppendLine();
-            _text.AppendLine(");");
-            _text.AppendLine();
+            _script.Remove((_script.Length - 3), 3);
+            _script.AppendLine();
+            _script.AppendLine(");");
+            _script.AppendLine();
 
             foreach (var index in table.Indexes)
             {
@@ -83,18 +81,16 @@ namespace Gigobyte.Daterpillar.TextTransformation
                 Create(index);
             }
 
-            _text.AppendLine();
+            _script.AppendLine();
         }
 
         public void Create(Column column)
         {
-            if (column.AutoIncrement) column.IsNullable = false;
-
             string dataType = _typeResolver.GetName(column.DataType);
             string notNull = (column.IsNullable ? string.Empty : " NOT NULL");
             string autoIncrement = (column.AutoIncrement ? $" PRIMARY KEY IDENTITY(1, 1)" : string.Empty);
 
-            _text.AppendLine($"ALTER TABLE {_schemaName}[dbo].[{column.TableRef}] ADD [{column.Name}] {dataType}{notNull}{autoIncrement};");
+            _script.AppendLine($"ALTER TABLE [{column.TableRef.SchemaRef.Name}].[dbo].[{column.TableRef.Name}] ADD [{column.Name}] {dataType}{notNull}{autoIncrement};");
         }
 
         public void Create(Index index)
@@ -103,23 +99,23 @@ namespace Gigobyte.Daterpillar.TextTransformation
             string columns = string.Join(", ", index.Columns.Select(x => ($"[{x.Name}] {x.Order}")));
             index.Name = (string.IsNullOrEmpty(index.Name) ? $"{index.Table}_idx{_seed++}" : index.Name).ToLower();
 
-            _text.AppendLine($"CREATE{unique}INDEX [{index.Name}] ON {_schemaName}[dbo].[{index.Table}] ({columns});");
+            _script.AppendLine($"CREATE{unique}INDEX [{index.Name}] ON [{index.TableRef.SchemaRef.Name}].[dbo].[{index.Table}] ({columns});");
         }
 
         public void Create(ForeignKey foreignKey)
         {
             if (string.IsNullOrEmpty(foreignKey.Name)) foreignKey.Name = $"{foreignKey.LocalTable}_{foreignKey.LocalColumn}_to_{foreignKey.ForeignTable}_{foreignKey.ForeignColumn}_fkey{_seed++}";
-            _text.AppendLine($"ALTER TABLE {_schemaName}[dbo].[{foreignKey.LocalTable}]  WITH CHECK ADD  CONSTRAINT [{foreignKey.Name}] FOREIGN KEY ([{foreignKey.LocalColumn}]) REFERENCES {_schemaName}[dbo].[{foreignKey.ForeignTable}] ([{foreignKey.ForeignColumn}]) ON UPDATE {foreignKey.OnUpdate} ON DELETE {foreignKey.OnDelete};");
+            _script.AppendLine($"ALTER TABLE [{foreignKey.TableRef.SchemaRef.Name}].[dbo].[{foreignKey.LocalTable}]  WITH CHECK ADD  CONSTRAINT [{foreignKey.Name}] FOREIGN KEY ([{foreignKey.LocalColumn}]) REFERENCES [{foreignKey.TableRef.SchemaRef.Name}].[dbo].[{foreignKey.ForeignTable}] ([{foreignKey.ForeignColumn}]) ON UPDATE {foreignKey.OnUpdate} ON DELETE {foreignKey.OnDelete};");
         }
 
         public void Drop(Schema schema)
         {
-            _text.AppendLine($"DROP DATABASE [{schema.Name}];");
+            _script.AppendLine($"DROP DATABASE [{schema.Name}];");
         }
 
         public void Drop(Table table)
         {
-            _text.AppendLine($"DROP TABLE {_schemaName}[dbo].[{table.Name}];");
+            _script.AppendLine($"DROP TABLE [{table.SchemaRef.Name}].[dbo].[{table.Name}];");
         }
 
         public void Drop(Column column)
@@ -129,17 +125,17 @@ namespace Gigobyte.Daterpillar.TextTransformation
             foreach (var index in schema.GetIndexes()) RemoveAllReferencesToColumn(index, column.Name);
             foreach (var constraint in schema.GetForeignKeys()) RemoveAllReferencesToColumn(constraint, column.TableRef.Name, column.Name);
 
-            _text.AppendLine($"ALTER TABLE {_schemaName}[dbo].[{column.TableRef.Name}] DROP COLUMN [{column.Name}];");
+            _script.AppendLine($"ALTER TABLE [{column.TableRef.SchemaRef.Name}].[dbo].[{column.TableRef.Name}] DROP COLUMN [{column.Name}];");
         }
 
         public void Drop(Index index)
         {
-            _text.AppendLine($"DROP INDEX {_schemaName}[dbo].[{index.Name}];");
+            _script.AppendLine($"DROP INDEX [{index.TableRef.SchemaRef.Name}].[dbo].[{index.Name}];");
         }
 
         public void Drop(ForeignKey foreignKey)
         {
-            _text.AppendLine($"ALTER TABLE {_schemaName}[dbo].[{foreignKey.LocalTable}] DROP CONSTRAINT [{foreignKey.Name}];");
+            _script.AppendLine($"ALTER TABLE [{foreignKey.TableRef.SchemaRef.Name}].[dbo].[{foreignKey.LocalTable}] DROP CONSTRAINT [{foreignKey.Name}];");
         }
 
         public void AlterTable(Column oldColumn, Column newColumn)
@@ -150,7 +146,7 @@ namespace Gigobyte.Daterpillar.TextTransformation
             string notNull = (newColumn.IsNullable ? string.Empty : " NOT NULL");
             string autoIncrement = (newColumn.AutoIncrement ? $" PRIMARY KEY IDENTITY(1, 1)" : string.Empty);
 
-            _text.AppendLine($"ALTER TABLE [{oldColumn.TableRef.Name}] ALTER COLUMN [{oldColumn.Name}] {dataType}{notNull}{autoIncrement};");
+            _script.AppendLine($"ALTER TABLE [{oldColumn.TableRef.Name}] ALTER COLUMN [{oldColumn.Name}] {dataType}{notNull}{autoIncrement};");
         }
 
         public void AlterTable(Table oldTable, Table newTable)
@@ -160,16 +156,15 @@ namespace Gigobyte.Daterpillar.TextTransformation
 
         public string GetContent()
         {
-            return _text.ToString();
+            return _script.ToString();
         }
 
         #region Private Members
 
         private readonly ITypeNameResolver _typeResolver;
         private readonly MSSQLScriptBuilderSettings _settings;
-        private readonly StringBuilder _text = new StringBuilder();
+        private readonly StringBuilder _script = new StringBuilder();
 
-        private string _schemaName;
         private int _seed = 1;
 
         private void AppendToTable(Column column)
@@ -180,7 +175,7 @@ namespace Gigobyte.Daterpillar.TextTransformation
             string notNull = (column.IsNullable ? string.Empty : " NOT NULL");
             string autoIncrement = (column.AutoIncrement ? $" PRIMARY KEY IDENTITY(1, 1)" : string.Empty);
 
-            _text.AppendLine($"\t[{column.Name}] {dataType}{notNull}{autoIncrement},");
+            _script.AppendLine($"\t[{column.Name}] {dataType}{notNull}{autoIncrement},");
         }
 
         private void AppendToTable(ForeignKey foreignKey)
@@ -189,7 +184,7 @@ namespace Gigobyte.Daterpillar.TextTransformation
             string onDelete = (foreignKey.OnDeleteRule != ForeignKeyRule.RESTRICT ? $" ON DELETE {foreignKey.OnDeleteRule}" : string.Empty);
             foreignKey.Name = (string.IsNullOrEmpty(foreignKey.Name) ? $"{foreignKey.LocalTable}_{foreignKey.LocalColumn}_to_{foreignKey.ForeignTable}_{foreignKey.ForeignColumn}_fkey{_seed++}" : foreignKey.Name).ToLower();
 
-            _text.AppendLine($"\tCONSTRAINT [{foreignKey.Name}] FOREIGN KEY ([{foreignKey.LocalColumn}]) REFERENCES [{foreignKey.ForeignTable}]([{foreignKey.ForeignColumn}]){onUpdate}{onDelete},");
+            _script.AppendLine($"\tCONSTRAINT [{foreignKey.Name}] FOREIGN KEY ([{foreignKey.LocalColumn}]) REFERENCES [{foreignKey.ForeignTable}]([{foreignKey.ForeignColumn}]){onUpdate}{onDelete},");
         }
 
         private void RemoveAllReferencesToColumn(Index index, string columnName)
