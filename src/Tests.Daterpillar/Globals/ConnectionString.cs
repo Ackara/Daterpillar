d88135470f463ca8d55b689088f08b1b55e4b36e
Ackara <ackara.dev@outlook.com>
2016-10-03 07:14:36 -0400

    Add connectionstring and connectionfactory.cs
    These files will help create database connections

---
+++ src/Tests.Daterpillar/Globals/ConnectionString.cs
@@ -0,0 +1,38 @@
+﻿using System.Linq;
+using System.Xml.Linq;
+
+namespace Tests.Daterpillar.Globals
+{
+    public static class ConnectionString
+    {
+        private static readonly string _configFile = "database.config.xml";
+
+        public static string GetMySQLServerConnectionString()
+        {
+            return GetConnectionString("mysql");
+        }
+
+        public static string GetSQLServerConnectionString()
+        {
+            return GetConnectionString("mssql");
+        }
+
+        internal static string GetConnectionString(string name)
+        {
+            var doc = XDocument.Load(_configFile);
+            var connectionStrings = doc.Element("connectionStrings");
+
+            var record = (from element in connectionStrings.Descendants("add")
+                          where element.Attribute("name").Value == name
+                          select element)
+                          .First();
+
+            var connStr = new System.Data.Common.DbConnectionStringBuilder();
+            connStr.Add("server", record.Attribute("server").Value);
+            connStr.Add("user", record.Attribute("user").Value);
+            connStr.Add("password", record.Attribute("password").Value);
+
+            return connStr.ConnectionString;
+        }
+    }
+}
---
+++ src/Tests.Daterpillar/Helper/ConnectionFactory.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Data;
+using System.IO;
+
+namespace Tests.Daterpillar.Helper
+{
+    public static class ConnectionFactory
+    {
+        public static IDbConnection CreateMySQLConnection(string database = null)
+        {
+            var connStr = new MySql.Data.MySqlClient.MySqlConnectionStringBuilder("");
+            if (!string.IsNullOrEmpty(database)) connStr.Database = database;
+
+            return new MySql.Data.MySqlClient.MySqlConnection(connStr.ConnectionString);
+        }
+
+        public static IDbConnection CreateMSSQLConnection(string database = null)
+        {
+            var connStr = new System.Data.SqlClient.SqlConnectionStringBuilder("");
+            if (!string.IsNullOrEmpty(database)) connStr.Add("database", database);
+
+            return new System.Data.SqlClient.SqlConnection(connStr.ConnectionString);
+        }
+
+        public static IDbConnection CreateSQLiteConnection(string filePath = "")
+        {
+            if (!File.Exists(filePath))
+            {
+                filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "daterpillar-test.db3");
+                System.Data.SQLite.SQLiteConnection.CreateFile(filePath);
+            }
+
+            var connStr = new System.Data.SQLite.SQLiteConnectionStringBuilder() { DataSource = filePath };
+            return new System.Data.SQLite.SQLiteConnection(connStr.ConnectionString);
+        }
+    }
+}
