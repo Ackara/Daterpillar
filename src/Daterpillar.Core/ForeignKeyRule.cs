e25e62d04ad1670e4efc32ecc3e0a53a285830db
Ackara <ackara.dev@outlook.com>
2016-10-05 05:43:54 -0400

    Add xmlenumattribute to values

--- src/Daterpillar.Core/ForeignKeyRule.cs
+++ src/Daterpillar.Core/ForeignKeyRule.cs
@@ -1,14 +1,25 @@
-﻿namespace Gigobyte.Daterpillar
+﻿using System.Xml.Serialization;
+
+namespace Gigobyte.Daterpillar
 {
     /// <summary>
     /// Foreign key collision rule.
     /// </summary>
     public enum ForeignKeyRule
     {
+        [XmlEnum]
         NONE,
+
+        [XmlEnum]
         CASCADE,
+
+        [XmlEnum]
         SET_NULL,
+
+        [XmlEnum]
         SET_DEFAULT,
+
+        [XmlEnum]
         RESTRICT
     }
 }
