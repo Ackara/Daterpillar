96d3db522b46221b286c484ab010bdba3cb8c07a
Ackara <ackara.dev@outlook.com>
2016-10-06 06:40:01 -0400

    Add knownfile.cs

---
+++ src/Tests.Daterpillar/Globals/KnownFile.cs
@@ -0,0 +1,15 @@
+﻿namespace Tests.Daterpillar.Globals
+{
+    public static class KnownFile
+    {
+        public const string XDDL = "xddl.xsd";
+        public const string x86SQLiteInterop = "x86\\SQLite.Interop.dll";
+        public const string x64SQLiteInterop = "x64\\SQLite.Interop.dll";
+
+        public const string SongCSV = "songs.csv";
+        public const string DataTypesCSV = "data_types.csv";
+        public const string MockSchemaXML = "mock-schema.xml";
+        public const string DataSoftXDDL = "datasoft.xddl.xml";
+        public const string TextFormatCSV = "text_formats.csv";
+    }
+}
