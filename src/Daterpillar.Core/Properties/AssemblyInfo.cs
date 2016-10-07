8f42cc803df088e3f2a1a63c6292423b19c7a1f8
Ackara <ackara.dev@outlook.com>
2016-10-07 04:25:58 -0400

    Update assembly version

--- src/Daterpillar.Core/Properties/AssemblyInfo.cs
+++ src/Daterpillar.Core/Properties/AssemblyInfo.cs
@@ -20,5 +20,5 @@ using System.Resources;
 //
 // You can specify all the values or you can default the Build and Revision Numbers by using the '*'
 // as shown below: [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.16280.1018.295")]
-[assembly: AssemblyFileVersion("2.16280.1018.294")]
+[assembly: AssemblyVersion("2.16281.0811.295")]
+[assembly: AssemblyFileVersion("2.16281.0811.294")]
--- src/Daterpillar.NET/Properties/AssemblyInfo.cs
+++ src/Daterpillar.NET/Properties/AssemblyInfo.cs
@@ -25,5 +25,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers by using the '*'
 // as shown below: [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.16280.1018.345")]
-[assembly: AssemblyFileVersion("2.16280.1018.345")]
+[assembly: AssemblyVersion("2.16281.0811.345")]
+[assembly: AssemblyFileVersion("2.16281.0811.345")]
