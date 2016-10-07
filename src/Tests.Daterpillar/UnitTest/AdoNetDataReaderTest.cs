181622771e647da1339dba8b9c7f7e4fbb6d664f
Ackara <ackara.dev@outlook.com>
2016-10-07 04:27:15 -0400

    Make test more atomic

--- src/Tests.Daterpillar/UnitTest/AdoNetDataReaderTest.cs
+++ src/Tests.Daterpillar/UnitTest/AdoNetDataReaderTest.cs
@@ -1,6 +1,7 @@
 ﻿using Gigobyte.Daterpillar.Data;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using System.Runtime.Serialization;
 
 namespace Tests.Daterpillar.UnitTest
 {
@@ -20,16 +21,133 @@ namespace Tests.Daterpillar.UnitTest
             var price = Convert.ToDecimal(TestContext.DataRow["Price"]);
             var onDevice = Convert.ToBoolean(TestContext.DataRow["On_Device"]);
 
-            var returnType = typeof(Sample.Song);
+            var returnType = typeof(Song);
             var sut = new AdoNetDataReader();
 
             // Act
-            var entity = (Sample.Song)sut.CreateInstance(returnType, TestContext.DataRow);
+            var entity = (Song)sut.CreateInstance(returnType, TestContext.DataRow);
 
             // Assert
             Assert.AreEqual((name == string.Empty ? null : name), entity.Name);
             Assert.AreEqual(price, entity.Price);
             Assert.AreEqual(onDevice, entity.OnDevice);
         }
+
+        #region Private
+
+        /// <summary>
+        /// Represents the [song] table.
+        /// </summary>
+        [DataContract]
+        [Table("song")]
+        public partial class Song : EntityBase
+        {
+            #region Constants
+
+            /// <summary>
+            /// The song table identifier.
+            /// </summary>
+            public const string Table = "song";
+
+            /// <summary>
+            /// The [song].[Id] column identifier.
+            /// </summary>
+            public const string IdColumn = "Id";
+
+            /// <summary>
+            /// The [song].[Album_Id] column identifier.
+            /// </summary>
+            public const string AlbumIdColumn = "Album_Id";
+
+            /// <summary>
+            /// The [song].[Artist_Id] column identifier.
+            /// </summary>
+            public const string ArtistIdColumn = "Artist_Id";
+
+            /// <summary>
+            /// The [song].[Genre_Id] column identifier.
+            /// </summary>
+            public const string GenreIdColumn = "Genre_Id";
+
+            /// <summary>
+            /// The [song].[Name] column identifier.
+            /// </summary>
+            public const string NameColumn = "Name";
+
+            /// <summary>
+            /// The [song].[Length] column identifier.
+            /// </summary>
+            public const string LengthColumn = "Length";
+
+            /// <summary>
+            /// The [song].[Price] column identifier.
+            /// </summary>
+            public const string PriceColumn = "Price";
+
+            /// <summary>
+            /// The [song].[On_Device] column identifier.
+            /// </summary>
+            public const string OnDeviceColumn = "On_Device";
+
+            #endregion Constants
+
+            /// <summary>
+            /// Get or set the [song].[Id] column value.
+            /// </summary>
+            [DataMember]
+            [Column("Id", IsKey = true, AutoIncrement = true)]
+            public virtual int Id { get; set; }
+
+            /// <summary>
+            /// Get or set the [song].[Album_Id] column value.
+            /// </summary>
+            [DataMember]
+            [Column("Album_Id")]
+            public virtual int AlbumId { get; set; }
+
+            /// <summary>
+            /// Get or set the [song].[Artist_Id] column value.
+            /// </summary>
+            [DataMember]
+            [Column("Artist_Id")]
+            public virtual int ArtistId { get; set; }
+
+            /// <summary>
+            /// Get or set the [song].[Genre_Id] column value.
+            /// </summary>
+            [DataMember]
+            [Column("Genre_Id")]
+            public virtual int GenreId { get; set; }
+
+            /// <summary>
+            /// Get or set the [song].[Name] column value.
+            /// </summary>
+            [DataMember]
+            [Column("Name")]
+            public virtual string Name { get; set; }
+
+            /// <summary>
+            /// Get or set the [song].[Length] column value.
+            /// </summary>
+            [DataMember]
+            [Column("Length")]
+            public virtual decimal Length { get; set; }
+
+            /// <summary>
+            /// Get or set the [song].[Price] column value.
+            /// </summary>
+            [DataMember]
+            [Column("Price")]
+            public virtual decimal Price { get; set; }
+
+            /// <summary>
+            /// Get or set the [song].[On_Device] column value.
+            /// </summary>
+            [DataMember]
+            [Column("On_Device")]
+            public virtual bool OnDevice { get; set; }
+        }
+
+        #endregion Private
     }
 }
--- src/Tests.Daterpillar/UnitTest/SqlWriterTest.cs
+++ src/Tests.Daterpillar/UnitTest/SqlWriterTest.cs
@@ -1,8 +1,9 @@
-﻿using Gigobyte.Daterpillar.Linq;
+﻿using Gigobyte.Daterpillar.Data;
+using Gigobyte.Daterpillar.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Runtime.CompilerServices;
-using Tests.Daterpillar.Sample;
+using System.Runtime.Serialization;
 
 namespace Tests.Daterpillar.UnitTest
 {
@@ -110,6 +111,116 @@ namespace Tests.Daterpillar.UnitTest
                 GenreId = 1,
                 OnDevice = true
             };
+        }
+
+        [DataContract]
+        [Table("song")]
+        public partial class Song : EntityBase
+        {
+            #region Constants
+
+            /// <summary>
+            /// The song table identifier.
+            /// </summary>
+            public const string Table = "song";
+
+            /// <summary>
+            /// The [song].[Id] column identifier.
+            /// </summary>
+            public const string IdColumn = "Id";
+
+            /// <summary>
+            /// The [song].[Album_Id] column identifier.
+            /// </summary>
+            public const string AlbumIdColumn = "Album_Id";
+
+            /// <summary>
+            /// The [song].[Artist_Id] column identifier.
+            /// </summary>
+            public const string ArtistIdColumn = "Artist_Id";
+
+            /// <summary>
+            /// The [song].[Genre_Id] column identifier.
+            /// </summary>
+            public const string GenreIdColumn = "Genre_Id";
+
+            /// <summary>
+            /// The [song].[Name] column identifier.
+            /// </summary>
+            public const string NameColumn = "Name";
+
+            /// <summary>
+            /// The [song].[Length] column identifier.
+            /// </summary>
+            public const string LengthColumn = "Length";
+
+            /// <summary>
+            /// The [song].[Price] column identifier.
+            /// </summary>
+            public const string PriceColumn = "Price";
+
+            /// <summary>
+            /// The [song].[On_Device] column identifier.
+            /// </summary>
+            public const string OnDeviceColumn = "On_Device";
+
+            #endregion Constants
+
+            /// <summary>
+            /// Get or set the [song].[Id] column value.
+            /// </summary>
+            [DataMember]
+            [Column("Id", IsKey = true, AutoIncrement = true)]
+            public virtual int Id { get; set; }
+
+            /// <summary>
+            /// Get or set the [song].[Album_Id] column value.
+            /// </summary>
+            [DataMember]
+            [Column("Album_Id")]
+            public virtual int AlbumId { get; set; }
+
+            /// <summary>
+            /// Get or set the [song].[Artist_Id] column value.
+            /// </summary>
+            [DataMember]
+            [Column("Artist_Id")]
+            public virtual int ArtistId { get; set; }
+
+            /// <summary>
+            /// Get or set the [song].[Genre_Id] column value.
+            /// </summary>
+            [DataMember]
+            [Column("Genre_Id")]
+            public virtual int GenreId { get; set; }
+
+            /// <summary>
+            /// Get or set the [song].[Name] column value.
+            /// </summary>
+            [DataMember]
+            [Column("Name")]
+            public virtual string Name { get; set; }
+
+            /// <summary>
+            /// Get or set the [song].[Length] column value.
+            /// </summary>
+            [DataMember]
+            [Column("Length")]
+            public virtual decimal Length { get; set; }
+
+            /// <summary>
+            /// Get or set the [song].[Price] column value.
+            /// </summary>
+            [DataMember]
+            [Column("Price")]
+            public virtual decimal Price { get; set; }
+
+            /// <summary>
+            /// Get or set the [song].[On_Device] column value.
+            /// </summary>
+            [DataMember]
+            [Column("On_Device")]
+            public virtual bool OnDevice { get; set; }
         }
 
         #endregion Private Members
