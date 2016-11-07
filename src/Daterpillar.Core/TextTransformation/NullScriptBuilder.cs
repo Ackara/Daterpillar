f47b81e7e06a7b88453afc10b704a4e66bb8f562
Ackara <ackara.dev@outlook.com>
2016-11-07 16:24:57 -0400

    Rename nullTemplate.cs to nullScriptBuilder

---
+++ src/Daterpillar.Core/TextTransformation/NullScriptBuilder.cs
@@ -0,0 +1,70 @@
+﻿namespace Gigobyte.Daterpillar.TextTransformation
+{
+    public class NullTemplate : IScriptBuilder
+    {
+        public void AlterTable(Column oldColumn, Column newColumn)
+        {
+        }
+
+        public void AlterTable(Table oldTable, Table newTable)
+        {
+        }
+
+        public void Append(string text)
+        {
+        }
+
+        public void AppendLine(string text)
+        {
+        }
+
+        public void Clear()
+        {
+        }
+
+        public void Create(Index index)
+        {
+        }
+
+        public void Create(ForeignKey foreignKey)
+        {
+        }
+
+        public void Create(Column column)
+        {
+        }
+
+        public void Create(Table table)
+        {
+        }
+
+        public void Create(Schema schema)
+        {
+        }
+
+        public void Drop(Column column)
+        {
+        }
+
+        public void Drop(ForeignKey foreignKey)
+        {
+        }
+
+        public void Drop(Index index)
+        {
+        }
+
+        public void Drop(Table table)
+        {
+        }
+
+        public void Drop(Schema schema)
+        {
+        }
+
+        public string GetContent()
+        {
+            return string.Empty;
+        }
+    }
+}
