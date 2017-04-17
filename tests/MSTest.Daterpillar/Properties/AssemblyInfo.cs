95e6c7c90dc67a453cb5780b9e6f183b2c1f04d2
Ackara <ackara.dev@outlook.com>
2017-04-17 06:23:48 -0400

    Revert mstest assembly info

--- tests/MSTest.Daterpillar/Properties/AssemblyInfo.cs
+++ tests/MSTest.Daterpillar/Properties/AssemblyInfo.cs
@@ -1,5 +1,4 @@
 using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("MSTest.Daterpillar")]
@@ -10,11 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
 [assembly: ComVisible(false)]
-
 [assembly: Guid("5d070576-6547-4991-be19-a3ba9621e77a")]
-
-// [assembly: AssemblyVersion("4.0.1")]
-[assembly: AssemblyVersion("4.0.1")]
-[assembly: AssemblyFileVersion("4.0.1")]
+[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyFileVersion("1.0.0")]
