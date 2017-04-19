feb9b26848937696bb40072ac9395ecc9721aea5
Ackara <ackara.dev@outlook.com>
2017-04-18 22:41:20 -0400

    Add ConnectionFactory.cs and app.config mstest project

---
+++ tests/MSTest.Daterpillar/ConnectionFactory.cs
@@ -0,0 +1,37 @@
+﻿using System.Data;
+
+namespace MSTest.Daterpillar
+{
+    public class ConnectionFactory
+    {
+        public static string GetMSSQLConnectionString()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public static string GetMySqlConnectionString()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public static string GetSQLiteConnectionString()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public static IDbConnection CreateMSSQLConnection()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public static IDbConnection CreateMySqlConnection()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public static IDbConnection CreateSQLiteConnection()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
