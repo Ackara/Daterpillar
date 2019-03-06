8d59d0e676bfdac48097be442d170c72ffcf3e7d
Ackara <ackara.dev@outlook.com>
2019-03-06 08:13:36 -0400

    Cleanup tests

--- tests/Daterpillar.MSTest/Startup.cs
+++ tests/Daterpillar.MSTest/Startup.cs
@@ -1,11 +1,27 @@
-﻿
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+using System.Linq;
 
-[TestClass]
-public class Startup
+namespace Acklann.Daterpillar
 {
-	public static void Initialize(TestContext context)
-	{
-        Acklann.Diffa.Resolution.TestContext.ProjectDirectory = @"C:\Users\Ackeem\Projects\Acklann\Daterpillar\tests\Daterpillar.MSTest";
-	}
+    [TestClass]
+    public class Startup
+    {
+        [AssemblyInitialize]
+        public static void Initialize(TestContext context)
+        {
+        }
+
+        [AssemblyCleanup]
+        public static void Cleanup()
+        {
+            var mssqlFiles = from f in Directory.EnumerateFiles(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile))
+                             where f.StartsWith("dtp-") && (f.EndsWith(".mdf") || f.EndsWith(".ldf"))
+                             select f;
+
+            foreach (string filePath in mssqlFiles)
+                File.Delete(filePath);
+        }
+    }
 }
