0d764114b0b9accb8c0caef207333df551b88f57
Ackara <ackara.dev@outlook.com>
2022-06-06 23:35:14 -0400

    improve generate command

--- src/Daterpillar.Tool/Commands/GenerateCommand.cs
+++ src/Daterpillar.Tool/Commands/GenerateCommand.cs
@@ -1,7 +1,8 @@
 using Acklann.Daterpillar.Modeling;
 using CommandLine;
+using CommandLine.Text;
 using System;
-using System.IO;
+using System.Collections.Generic;
 using System.Xml.Schema;
 
 namespace Acklann.Daterpillar.Tool.Commands
@@ -9,25 +10,25 @@ namespace Acklann.Daterpillar.Tool.Commands
     [Verb("generate", HelpText = "Produces a migration script from the old schema to the new schema.")]
     public class GenerateCommand : ICommand
     {
-        [Option('f', "old-schema", Required = true)]
+        [Option('f', "old-schema", Required = true, HelpText = "The path of the old schema file.")]
         public string OldSchemaPath { get; set; }
 
-        [Option('t', "new-schema", Required = true)]
+        [Option('t', "new-schema", Required = true, HelpText = "The path of the new schema file.")]
         public string NewSchemaPath { get; set; }
 
-        [Option('o', "output", Required = true)]
+        [Option('o', "output", Required = true, HelpText = "The path of the generated migration script.")]
         public string OutputFile { get; set; }
 
-        [Option('l', "language", Required = true)]
+        [Option('l', "language", Required = true, HelpText = "The SQL language")]
         public Acklann.Daterpillar.Language Language { get; set; }
 
         public int Execute()
         {
-            if (!File.Exists(OldSchemaPath)) throw new FileNotFoundException($"Could not find file at '{OldSchemaPath}'.");
-            if (!File.Exists(NewSchemaPath)) throw new FileNotFoundException($"Could not find file at '{NewSchemaPath}'.");
+            if (string.IsNullOrWhiteSpace(NewSchemaPath)) throw new ArgumentNullException(nameof(NewSchemaPath));
+            if (string.IsNullOrWhiteSpace(OldSchemaPath)) throw new ArgumentNullException(nameof(OldSchemaPath));
             if (string.IsNullOrWhiteSpace(OutputFile)) throw new ArgumentNullException(nameof(OutputFile));
-            if (!Schema.TryLoad(OldSchemaPath, out Schema oldSchema, PrintErrors)) throw new InvalidDataException();
-            if (!Schema.TryLoad(NewSchemaPath, out Schema newSchema, PrintErrors)) throw new InvalidDataException();
+            if (!Schema.TryLoad(OldSchemaPath, out Schema oldSchema, PrintErrors)) oldSchema = new Schema();
+            if (!Schema.TryLoad(NewSchemaPath, out Schema newSchema, PrintErrors)) newSchema = new Schema();
 
             new Migrator().GenerateMigrationScript(Language, oldSchema, newSchema, OutputFile);
             return 0;
@@ -36,6 +37,21 @@ namespace Acklann.Daterpillar.Tool.Commands
         private void PrintErrors(XmlSeverityType level, XmlSchemaException error)
         {
             Console.Error.WriteLine($"[{level}] {error.Message}");
+        }
+
+        [Usage(ApplicationAlias = "daterpillar")]
+        public static IEnumerable<Example> Examples
+        {
+            get
+            {
+                yield return new Example("create migration script", new GenerateCommand
+                {
+                    OldSchemaPath = @"C:\old-schema.xml",
+                    NewSchemaPath = @"C:\new-schema.xml",
+                    OutputFile = @"C:\V1.0__init.sql",
+                    Language = Language.MySQL
+                });
+            }
         }
     }
 }
